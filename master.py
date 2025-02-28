07ade1edfa92e51a2290ac01c73bc6b9da2f006c
миша <osipenko.ma@dvfu.ru>
2025-02-28 23:28:34 +1000

    First commit + test notify

---
+++ master.py
@@ -0,0 +1,19 @@
+import telebot, sqlite3
+
+with open("bot_token.txt", "r") as file:
+    token = file.read()
+
+bot = telebot.TeleBot(token, parse_mode="HTML")
+
+# TODO: add logic
+def isFirstMessage(message):
+    return True
+
+# TODO: add user to db
+@bot.message_handler(func=lambda message: isFirstMessage(message) == True)
+def firstMessageHandler(message):
+    with open("greeting_text.txt", 'r') as text:
+        bot.send_message(message.from_user.id, text.read())
+
+print('initialized')
+bot.infinity_polling()
